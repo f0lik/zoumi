a35deda6bc1ecdfe8264d8eeca21c8d14c6413e6
Pavel <fol.pavel@gmail.com>
2018-01-03 11:14:13 +0100

    Connect to DB, add index controller and content

--- src/main/kotlin/cz/f0lik/zoumi/controller/AppController.kt
+++ src/main/kotlin/cz/f0lik/zoumi/controller/AppController.kt
@@ -1,13 +1,20 @@
 package cz.f0lik.zoumi.controller
 
+import cz.f0lik.zoumi.repository.ArticleRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class AppController {
+    @Autowired
+    var articleRepository: ArticleRepository? = null
 
     @GetMapping(value = ["/"])
-    fun index(): String {
+    fun index(model: Model): String {
+        val size = articleRepository!!.findAll().size
+        model.addAttribute("artCount", size)
         return "index"
     }
 }
