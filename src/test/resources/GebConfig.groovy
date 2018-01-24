43ce76eabdf939e5c19bea21af683164eab9e916
Pavel <fol.pavel@gmail.com>
2018-01-24 20:59:47 +0100

    set HtmlUnitDriver for testing without gui

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,12 +1,12 @@
 import io.github.bonigarcia.wdm.ChromeDriverManager
-import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
 
 baseUrl = 'http://localhost:8090/'
 ChromeDriverManager.getInstance().setup()
 timeout = 15
 driver = {
-    def driver = new ChromeDriver()
-    println "driver"
+    def driver = new HtmlUnitDriver()
+
     driver.manage().window().maximize()
     driver
 }
