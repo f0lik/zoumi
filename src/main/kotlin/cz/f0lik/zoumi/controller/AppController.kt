79f5e57169a4cdef94b3f644e2f6b70f80a04a39
Pavel <fol.pavel@gmail.com>
2018-02-18 13:03:41 +0100

    use shutdown hook from actuator instead

--- src/main/kotlin/cz/f0lik/zoumi/controller/AppController.kt
+++ src/main/kotlin/cz/f0lik/zoumi/controller/AppController.kt
@@ -1,13 +1,11 @@
 package cz.f0lik.zoumi.controller
 
 import cz.f0lik.zoumi.repository.ArticleRepository
-import cz.f0lik.zoumi.utils.ShutdownManager
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 class AppController {
@@ -22,10 +20,5 @@ class AppController {
         val size = articleRepository!!.findAll().size
         model.addAttribute("artCount", size)
         return "index"
-    }
-
-    @PostMapping(value = ["/shtdw"])
-    fun shutdown() {
-        ShutdownManager.performShutdown(appContext)
     }
 }
