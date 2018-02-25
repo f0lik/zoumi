6d2b7df09cf4db01652d0fb89a56f6eead602e2d
Pavel <fol.pavel@gmail.com>
2018-02-25 12:21:01 +0100

    added tests/prod properties

--- src/test/groovy/integration/pages/ZMainPage.groovy
+++ src/test/groovy/integration/pages/ZMainPage.groovy
@@ -3,7 +3,7 @@ package integration.pages
 import geb.Page
 
 class ZMainPage extends Page {
-    static url = 'http://localhost:8090/'
+    static url = 'http://localhost:8091/'
     static at = { title == "Home | Zoumi.cz" }
     static content = {
         heading { $("h1").text() }
