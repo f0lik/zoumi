30ddd4c99ad5147c1b3683187e07386773796088
Pavel <fol.pavel@gmail.com>
2018-05-13 18:22:59 +0200

    start script updated

--- src/main/resources/scripts/start.sh
+++ src/main/resources/scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nohup java -Ddbpasswd=<databasePassword> -Dspring.profiles.active=prod -DdbUrl=<sourceDbUrl> -jar *.jar --server.port=9090 > nohup2.out&
+nohup java -Ddbuser=<databaseUser> -Ddbpasswd=<databasePassword> -Dspring.profiles.active=prod -DdbUrl=<sourceDbUrl> -jar *.jar --server.port=9090 > nohup2.out&
